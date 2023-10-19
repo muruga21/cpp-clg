db257b80d7532709cdea817970dca7c8f59b16f8
muruga21 <murugaperumal21062004@gmail.com>
2023-10-19 14:46:54 +0530

    kmp algo

--- kmpalgo.cpp
+++ kmpalgo.cpp
@@ -1,27 +1,70 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<string.h>
+
 using namespace std;
 
-int main()
-{
-    string text = "aaabbbabbba";
-    string pattern = "onion";
-    int m = pattern.size();
-    vector<int> lps(m,0);
-    int len = 1 ; 
+
+
+void prefixlps(char *pattern,int m,int *lps){
+
+    int len = 0; //variable to assist on traversal
+    lps[0] = 0;
     int i = 1;
-    while(i<m){
+
+    while(i < m){ //find a match,increment
         if(pattern[i] == pattern[len]){
-            lps[i] = len;
+
             len++;
+            lps[i] = len;
             i++;
+        }
+        else{ //match not found and len not equal
+            if(len != 0){
+                len = lps[len-1];
+            }
+            else{
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+
+    for (int z=0;z<m;z++){
+    cout<<lps[z]<<" ";}
+}
+
+void kmpsearch(char *text,char *pattern){
+
+    int n = strlen(text);
+    int m = strlen(pattern);
+    int lps[m];
+    prefixlps(pattern,m,lps);
+    int i = 0,j = 0;
+    while((n - i) >= (m - j)){
+        if(pattern[j] == text[i]){
+            i++,j++;
         }
         else{
-            lps[i]=0;
-            i++;
+            if(j!=0){
+                j = lps[j-1];
+            }
+            else{
+                i=i+1;
+            }
         }
+        if(j==m){
+            cout << "Pattern is found"<<" ";
+            j = lps[j-1];
+        }
+
     }
-    for(auto x : lps){
-        cout<<x;
-    }
+}
+
+int main(){
+
+    char text[] = "ababcabcababaab";
+    char pattern[] = "baab";
+    kmpsearch(text,pattern);
     return 0;
+
 }
